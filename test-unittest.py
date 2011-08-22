99db95892a74a0db4b1668a5cd5e5744e9e40e53
julian zhong <julian.ylzhong@gmail.com>
2011-08-23 06:23:09 +1000

    upgrade unittest cases

--- test-unittest.py
+++ test-unittest.py
@@ -2,23 +2,24 @@ from teamcity.unittestpy import TeamcityTestRunner
 import unittest
 
 class TestTeamcityMessages(unittest.TestCase):
-    def testPass(self):
-        assert 1 == 1
-    
-    def testAssertEqual(self):
-        self.assertEqual("1", "2")
 
-    def testAssertEqualMsg(self):
-        self.assertEqual("1", "2", "message")
-    
-    def testAssert(self):
-        self.assert_(False)
-    
-    def testFail(self):
-        self.fail("failed")
-        
-    def testException(self):
-        raise Exception("some exception")
+  def testPass(self):
+    self.assertEqual(1, 1, 'Error: testPass')
+
+  def testAssertEqual(self):
+    self.assertEqual(1, 2, 'Error: testAssertEqual')
+
+  def testAssertEqualMsg(self):
+    self.assertEqual("1", "2", 'Error: testAssertEqualMsg')
+
+  def testAssert(self):
+    self.assertTrue(False)
+
+  def testFail(self):
+    self.fail("SET it fail")
+
+  def testException(self):
+    raise Exception("some exception")
 
 if __name__ == '__main__':
-    unittest.main(testRunner=TeamcityTestRunner())
+  unittest.main(testRunner=TeamcityTestRunner())
