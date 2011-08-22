77f72f0e10c3043dc1b3f22a75bc9b298bbe7d7b
julian zhong <julian.ylzhong@gmail.com>
2011-08-23 09:32:23 +1000

    add a new test case into test suite

--- test_suite_try.py
+++ test_suite_try.py
@@ -1,19 +1,16 @@
 import unittest
 from test_unittest import TestTeamcityMessages
+from test_copy import TestTeamcityCopy
 
 def suite():
-
     suite = unittest.TestSuite()
-
     suite.addTest (TestTeamcityMessages())
+    suite.addTest (TestTeamcityCopy())
 
     return suite
 
 if __name__ == '__main__':
-
     runner = unittest.TextTestRunner()
-
     test_suite = suite()
-
     runner.run (test_suite)
   
