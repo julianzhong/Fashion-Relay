0f409ee29cb5186d3b5e5ccff3e09fe51d5e7b10
julian zhong <julian.ylzhong@gmail.com>
2011-08-23 09:41:17 +1000

    change method names, and take off TeamCityTestRunner

--- test_copy.py
+++ test_copy.py
@@ -1,17 +1,18 @@
-from teamcity.unittestpy import TeamcityTestRunner
+#from teamcity.unittestpy import TeamcityTestRunner
 import unittest
 
 # COPY = sample of using teamcity
 class TestTeamcityCopy(unittest.TestCase):
 
-  def testPass(self):
+  def testPassCopy(self):
     self.assertEqual(1, 1, 'Error: testPass')
 
-  def testAssertEqual(self):
+  def testAssertEqualCopy(self):
     self.assertEqual(1, 2, 'Error: testAssertEqual')
 
-  def testAssertEqualMsg(self):
+  def testAssertEqualMsgCopy(self):
     self.assertEqual("1", "3", 'Error: testAssertEqualMsg')
 
 if __name__ == '__main__':
-  unittest.main(testRunner=TeamcityTestRunner())
+  unittest.main()
+  #unittest.main(testRunner=TeamcityTestRunner())
