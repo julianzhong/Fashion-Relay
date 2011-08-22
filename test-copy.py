0b240bf491b2e605478f2735dde304ff985b22e7
julian zhong <julian.ylzhong@gmail.com>
2011-08-23 06:40:38 +1000

    add a copy of test-unittest

---
+++ test-copy.py
@@ -0,0 +1,26 @@
+from teamcity.unittestpy import TeamcityTestRunner
+import unittest
+
+# sample of using teamcity
+class TestTeamcityMessages(unittest.TestCase):
+
+  def testPass(self):
+    self.assertEqual(1, 1, 'Error: testPass')
+
+  def testAssertEqual(self):
+    self.assertEqual(1, 2, 'Error: testAssertEqual')
+
+  def testAssertEqualMsg(self):
+    self.assertEqual("1", "3", 'Error: testAssertEqualMsg')
+
+  def testAssert(self):
+    self.assertTrue(False)
+
+  def testFail(self):
+    self.fail("Always fail")
+
+  def testException(self):
+    raise Exception("some exception")
+
+if __name__ == '__main__':
+  unittest.main(testRunner=TeamcityTestRunner())
